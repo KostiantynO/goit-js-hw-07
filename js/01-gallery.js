b5dca2b34a53a8fada1a3982a38d2d63b48becd6
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-10-13 03:53:38 +0300

    1st task basic markup created

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const createGalleryMarkup = array =>
+  array
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `,
+    )
+    .join('');
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const onModalOpen = e => {
+  e.preventDefault();
+};
+gallery.addEventListener('click', onModalOpen);
