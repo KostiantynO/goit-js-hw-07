42b3b6848dfb96ddc48cb47ba085380bdc02f08a
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-10-13 04:10:03 +0300

    1st task - modal opens and closes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,7 +22,29 @@ const gallery = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
 gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
+const modalMarkup = `
+<div class="gallery-modal">
+      <img class="gallery-modal__image" src="https://via.placeholder.com/640/480" alt="Description">
+      <p class="gallery-modal__description">Picture</p>
+</div>
+`;
+
+const instance = basicLightbox.create(modalMarkup, {
+  onClose: () => window.removeEventListener('keydown', onEscCloseModal),
+});
+
+const galleryModal = instance.element().querySelector('.gallery-modal');
+
 const onModalOpen = e => {
   e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  instance.show();
+  window.addEventListener('keydown', onEscCloseModal);
 };
+
 gallery.addEventListener('click', onModalOpen);
+
+const onEscCloseModal = e => e.code === 'Escape' && instance.close();
