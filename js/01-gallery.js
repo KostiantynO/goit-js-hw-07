77e601f6ae0177ce308bc369046018c6ecddcdf4
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-10-21 02:37:26 +0300

    2nd task thanks to kitty � :)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,18 +2,18 @@ import { galleryItems } from './gallery-items.js';
 const createGalleryMarkup = array =>
   array
     .map(
-      ({ preview, original, description }) => `
+      ({ preview, original, description }, i) => `
     <li class="gallery__item">
       <a class="gallery__link" href="${original}">
         <img
+          id="${'img' + i}"
           class="gallery__image"
           src="${preview}"
           data-source="${original}"
           alt="${description}"
         />
       </a>
-    </li>
-    `,
+    </li>`,
     )
     .join('');
 
@@ -26,24 +26,45 @@ const modalMarkup = `
 <div class="gallery-modal">
       <img class="gallery-modal__image" src="https://via.placeholder.com/640/480" alt="Description">
       <p class="gallery-modal__description"><span class="gallery-modal__span">Picture</span></p>
+      <button class="back-btn gallery-modal__btn" type="button">◀</button>
+      <button class="next-btn gallery-modal__btn" type="button">▶</button>
 </div>
 `;
 
 const instance = basicLightbox.create(modalMarkup, {
-  onClose: () => window.removeEventListener('keydown', onEscCloseModal),
+  onClose: () => {
+    document.removeEventListener('keydown', onEscCloseModal);
+    backBtn.removeEventListener('click', onBackBtnClickShowPreviousImg);
+    nextBtn.removeEventListener('click', onNextBtnClickShowNextImg);
+  },
 });
 
 const galleryModal = instance.element().querySelector('.gallery-modal');
-const getSrcForOriginalImg = ({ dataset: { source }, alt }) => {
-  const originalImg = galleryModal.firstElementChild;
-  const span = galleryModal.querySelector('.gallery-modal__span');
+const originalImg = galleryModal.firstElementChild;
+const span = galleryModal.querySelector('.gallery-modal__span');
+const nextBtn = galleryModal.querySelector('.next-btn');
+const backBtn = galleryModal.querySelector('.back-btn');
 
+const getSrcForOriginalImg = ({ id, dataset: { source }, alt }) => {
+  if (originalImg.id !== id) {
+    originalImg.id = id;
+    console.log('getSrcForOriginalImg ~ id', id);
+    console.log('getSrcForOriginalImg ~ originalImg.id', originalImg.id);
+  }
   originalImg.src = source;
   originalImg.alt = alt;
+  originalImg.classList.add('current-img');
   span.textContent = alt;
 };
 
 const onEscCloseModal = e => e.code === 'Escape' && instance.close();
+
+const onBackBtnClickShowPreviousImg = e => {
+  const currentImg = document.getElementById(`${originalImg.id}`);
+  const previousImg = document.getElementById(`${originalImg.id.slice(3) - 1}`);
+  getSrcForOriginalImg(previousImg);
+};
+const onNextBtnClickShowNextImg = e => {};
 
 const onModalOpen = e => {
   e.preventDefault();
@@ -54,7 +75,10 @@ const onModalOpen = e => {
   const previewImg = e.target;
   getSrcForOriginalImg(previewImg);
   instance.show();
+
   window.addEventListener('keydown', onEscCloseModal);
+  backBtn.addEventListener('click', onBackBtnClickShowPreviousImg);
+  nextBtn.addEventListener('click', onNextBtnClickShowNextImg);
 };
 
 gallery.addEventListener('click', onModalOpen);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createGalleryMarkup = array =>
+  array
+    .map(
+      ({ preview, original, description }, i) =>
+        `<a class="gallery__item" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+         </a>`,
+    )
+    .join('');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+// const galleryContainer = document.querySelector('.gallery');
+// const galleryMarkup = createGallery(galleryItems);
+// galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+// function createGallery(items) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<a class="gallery__item" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//       </a>`;
+//     })
+//     .join('');
+// }
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
