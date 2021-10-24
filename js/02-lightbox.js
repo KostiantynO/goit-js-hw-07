3e05f71ecb81c941ca811a8d51316a081929ac85
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-10-24 03:46:35 +0300

    updates 2nd task in 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,9 +7,12 @@ const createGalleryMarkup = array =>
   array
     .map(
       ({ preview, original, description }, i) =>
-        `<a class="gallery__item" href="${original}">
-           <img class="gallery__image" src="${preview}" alt="${description}" />
-         </a>`,
+        `<li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+           </a>
+         </li>
+        `,
     )
     .join('');
 
